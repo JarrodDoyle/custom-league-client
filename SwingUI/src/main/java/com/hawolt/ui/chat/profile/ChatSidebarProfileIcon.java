0aeab4abdf467fd545db7b7002b815978cfd8af4
Jarrod Doyle <thejarroddoyle@gmail.com>
2023-10-01 00:24:25 +0100

    Remove old XP bar related code from profile icon and simply draw the icon

--- SwingUI/src/main/java/com/hawolt/ui/chat/profile/ChatSidebarProfileIcon.java
+++ SwingUI/src/main/java/com/hawolt/ui/chat/profile/ChatSidebarProfileIcon.java
@@ -10,6 +10,7 @@ import com.hawolt.virtual.leagueclient.userinfo.UserInformation;
 import org.imgscalr.Scalr;
 
 import javax.imageio.ImageIO;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
@@ -21,51 +22,26 @@ import java.io.ByteArrayInputStream;
 
 public class ChatSidebarProfileIcon extends ChildUIComponent implements ResourceConsumer<BufferedImage, byte[]> {
     private final String ICON_BASE_URL = "https://raw.communitydragon.org/latest/game/assets/ux/summonericons/profileicon%s.png";
-
-    private final Color UNOBTAINED = new Color(231, 97, 97);
-    private final Color GAINED = new Color(93, 156, 89);
-
-    private final Font font = new Font("Arial", Font.BOLD, 16);
-    private final int ICON_SIZE = 70;
-    private int current, total, level;
+    private final int ICON_SIZE = 64;
     private BufferedImage icon;
 
     public ChatSidebarProfileIcon(UserInformation information, LayoutManager layout) {
         super(layout);
         this.setBackground(ColorPalette.accentColor);
         this.setPreferredSize(new Dimension(ICON_SIZE, ICON_SIZE));
-        if (!information.isLeagueAccountAssociated()) return;
-        this.current = 419;
-        this.total = 2193;
-        this.level = (int) information.getUserInformationLeagueAccount().getSummonerLevel();
+        this.setBorder(new EmptyBorder(8, 8, 8, 8));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        //g.setColor(ColorPalette.cardColor);
-        //g.fillRect(0,0,getWidth(),getHeight());
 
-        Dimension dimension = getSize();
-        g.setColor(UNOBTAINED);
-        g.fillRect(0, ICON_SIZE, dimension.width, getHeight() - ICON_SIZE);
-        double progress = ((double) current / (double) total);
-        int width = (int) Math.floor(progress * (dimension.width - 1));
-        g.setColor(GAINED);
-        g.fillRect(0, ICON_SIZE, width, getHeight() - ICON_SIZE);
-
-        Graphics2D graphics2D = (Graphics2D) g;
-        graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        graphics2D.setFont(font);
-        FontMetrics metrics = g.getFontMetrics();
-
-        int y = (dimension.height >> 1) + (metrics.getAscent() >> 1);
-
-
-        int levelStringWidth = metrics.stringWidth(String.valueOf(level));
-        //drawHighlightedText(g, dimension, String.valueOf(level), dimension.width - 7 - levelStringWidth, y);
-        if (icon == null) return;
-        g.drawImage(PaintHelper.circleize(icon, ColorPalette.CARD_ROUNDING, true, true, false, false), 0, 0, null);
+        int y = getHeight() - ICON_SIZE;
+        g.setColor(ColorPalette.backgroundColor);
+        PaintHelper.roundedSquare((Graphics2D) g, 0, y, ICON_SIZE, ICON_SIZE, ColorPalette.CARD_ROUNDING, true, true, true, true);
+        if (icon != null) {
+            g.drawImage(PaintHelper.circleize(icon, ColorPalette.CARD_ROUNDING), 0, y, null);
+        }
     }
 
     public void setIconId(long iconId) {
